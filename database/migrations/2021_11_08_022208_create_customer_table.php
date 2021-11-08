b6ede72135fa533cc60a6b45886ded4651399562
calvinsiahaan9 <calvinsiahaan911@gmail.com>
2021-11-08 10:22:21 +0700

    'tugas3'

---
+++ database/migrations/2021_11_08_022208_create_customer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('email',191);
+            $table->string('name',191);
+            $table->text('address');
+            $table->string('phone',191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
--- resources/views/admin.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('master')
-@section('content')
-    <h1>ABC 5 dasar</h1>
-@endsection
